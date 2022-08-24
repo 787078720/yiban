9407cf78c40effe7c65e487e040af95376438c37
月初 <85841713+787078720@users.noreply.github.com>
2022-08-24 13:33:58 +0800

    Update index.py

--- index.py
+++ index.py
@@ -7,12 +7,10 @@ import hashlib
 #-------------------------------------------------------变量信息----------------------------------------------------------------#
 
 zhh={
-    1:"2021532102",###第一个账号
-    2:"0000000",#######第二个账号以此类推如果只有一个账号删除这行
+    1:"202153210231",###第一个账号
 }
 mmm={
-    1: "2551",###第一个账号密码
-    2:"0000000",###第二个账号密码以此类推如果只有一个账号删除这行
+    1: "A2942219885",###第一个账号密码
 }
 n=0+1
 zs = len(zhh)
@@ -30,6 +28,7 @@ while n<zs+1:
     m = (j[9:])
     l = (j[5:9])
     kk = k + "a" + l + "b" + m
+    n=n+1
     # -------------------------------------------------登录提取cookie--------------------------------------------------------------#
     headersq = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.46',
@@ -51,113 +50,108 @@ while n<zs+1:
     ]
     def post():
         r = requests.post(urlq, data=dataq, headers=headersq).headers['Set-Cookie']
+        print(r)
         return r
     cooick = (post()[:43])
     cooick1 = (cooick[11:])
-    # -------------------------------------------------获取上次打卡记录josn--------------------------------------------------------------#
+    # ---------------- ---------------------------------获取上次打卡记录josn--------------------------------------------------------------#
+def get_xxx():
+    url="http://xggl.hnqczy.com/content/student/temp/zzdk/lastone?_t_s_=1661221850154"
+    headers = {'Host': 'xggl.hnqczy.com',
+               'Connection': 'keep-alive',
+               'Accept': '*/*',
+               'User-Agent': 'Mozilla/5.0 (Linux; Android 12; M2012K11AC Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36;webank/h5face;webank/1.0 yiban_android/5.0.12',
+               'X-Requested-With': 'XMLHttpRequest',
+               'Referer': 'http://xggl.hnqczy.com/wap/menu/student/temp/zzdk/_child_/edit?_t_s_=1661221849267',
+               'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+               }
     cookies = {
-        'JSESSIONID': cooick1,
+        'JSESSIONID':cooick1,
     }
-
-    headers1 = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.46',
-        'Host': 'xggl.hnqczy.com',
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Referer': 'http://xggl.hnqczy.com/wap/menu/student/temp/zzdk/_child_/detail/16538745236399409694?_t_s_=1653919292775',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    rsp =requests.get(url, headers,cookies=cookies).json()
+    return rsp
+dkdz=get_xxx()['dkdz']
+dkd=get_xxx()['dkd']
+jzdDz2=get_xxx()['jzdDz2']
+lxdh = get_xxx()["lxdh"]
+print(get_xxx())
+# ----------------------------------------------------------打卡--------------------------------------------------------------#
+def post_xxx():
+    url="http://xggl.hnqczy.com/content/student/temp/zzdk?_t_s_=1661311939332"
+    headers = {'Host': 'xggl.hnqczy.com',
+               'Connection': 'keep-alive',
+               'Accept': '*/*',
+               'X-Requested-With': 'XMLHttpRequest',
+               'User-Agent': 'Mozilla/5.0 (Linux; Android 12; M2012K11AC Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36;webank/h5face;webank/1.0 yiban_android/5.0.12',
+               'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+               'Origin': 'http://xggl.hnqczy.com',
+               'Referer': 'http://xggl.hnqczy.com/wap/menu/student/temp/zzdk/_child_/edit?_t_s_=1661311908508',
+               'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+               }
+    cookies = {
+        'JSESSIONID':cooick1,
     }
-
-    params1 = (
-        ('_t_s_', '1653919293104'),
-    )
-
-    response = requests.get('http://xggl.hnqczy.com/content/student/temp/zzdk/16538745236399409694', headers=headers1,
-                            params=params1, cookies=cookies)
-    json = response.json()
-    cjdz = json['jzdDz2']
-    lxdh = json["lxdh"]
-    xzrq = json["fxrq"]
-    hs = "1"
-    # ----------------------------------------------------------打卡--------------------------------------------------------------#
-    url2 = "http://xggl.hnqczy.com/content/student/temp/zzdk?_t_s_=" + rq
-    headers2 = {"Host": "xggl.hnqczy.com",
-                "Connection": "keep-alive",
-                "Content-Length": "1028",
-                "Accept": "*/*",
-                "X-Requested-With": "XMLHttpRequest",
-                "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.46',
-                "Content-Type": "application/x-www-form-urlencoded; charset\u003dUTF-8",
-                "Origin": "http://xggl.hnqczy.com",
-                "Referer": "http://xggl.hnqczy.com/wap/menu/student/temp/zzdk/_child_/edit?_t_s_\u003d1648091401446",
-                "Accept-Encoding": "gzip, deflate",
-                "Accept-Language": "zh-CN,zh;q\u003d0.9,en-US;q\u003d0.8,en;q\u003d0.7",
-                "Cookie": cooick
-                }
-    data2 = [
-        ('dkdz', '湖南省株洲市天元区天台路60'),
-        ('dkdzZb', '113.1404708,27.83356764'),
-        ('dkly', 'baidu'),
-        ('dkd', '湖南省株洲市'),
-        ('jzdValue', '430000,430200,430202'),
-        ('jzdSheng.dm', '430000'),
-        ('jzdShi.dm', '430200'),
-        ('jzdXian.dm', '430202'),
-        ('jzdDz', '湖南汽车工程职业学院'),
-        ('jzdDz2', cjdz),
-        ('lxdh', lxdh),
-        ('sfzx', '1'),
-        ('sfzx1', '在校'),
-        ('twM.dm', '01'),
-        ('tw1', '[35.0~37.2]正常'),
-        ('yczk.dm', '01'),
-        ('yczk1', '无症状'),
-        ('fbrq', xzrq),
-        ('jzInd', '0'),
-        ('jzYy', ''),
-        ('zdjg', ''),
-        ('fxrq', xzrq),
-        ('brStzk.dm', '01'),
-        ('brStzk1', '身体健康、无异常'),
-        ('brJccry.dm', '01'),
-        ('brJccry1', '未接触传染源'),
-        ('jrStzk.dm', '01'),
-        ('jrStzk1', '身体健康、无异常'),
-        ('jrJccry.dm', '01'),
-        ('jrJccry1', '未接触传染源'),
-        ('jkm', '1'),
-        ('jkm1', '绿色'),
-        ('xcm', '1'),
-        ('xcm1', '绿色'),
-        ('xgym', ''),
-        ('xgym1', ''),
-        ('hsjc', '1'),
-        ('hsjc1', hs),
-        ('bz', ''),
-        ('operationType', 'Create'),
-        ('dm', ''),
-    ]
-    def postt():
-        r = requests.post(url2, data=data2, headers=headers2)
-        u=r.json()
-        return u
-    print(postt())
-    uuu=postt()
-# --------------------------------------------------------信息推送-----------------------------------------------------------------#def tuisong():
-    n=n+1
-key= "SCT114092TdaLtRYOnxp0vLc0wwZAau4V6"#微信推送key这里    链接获取#https://sct.ftqq.com/sendkey
-api = "https://sc.ftqq.com/"+key+".send"
-data = {
-    "title":"打卡成功",
-     "desp": uuu
-}
-req = requests.post(api, data=data).json()
-print(req)
-print(tuisong())
+    post_data = str("dkdz="+dkdz+
+                    "&dkdzZb=111.795074%2C28.592226"
+                    "&dkly=yiban"
+                    "&zzdk_token=h01xr2"
+                    "&dkd="+dkd+
+                    "&jzdValue=430000%2C430200%2C430202"
+                    "&jzdSheng.dm=430000"
+                    "&jzdShi.dm=430200"
+                    "&jzdXian.dm=430202"
+                    "&jzdDz=湖南汽车工程职业学院"
+                    "&jzdDz2="+jzdDz2+
+                    "&lxdh="+lxdh+
+                    "&sfzx=1&sfzx1=%E5%9C%A8%E6%A0%A1"
+                    "&twM.dm=01"
+                    "&tw1=%5B35.0~37.2%5D%E6%AD%A3%E5%B8%B8"
+                    "&tw1M.dm="
+                    "&tw11="
+                    "&tw2M.dm="
+                    "&tw12="
+                    "&tw3M.dm="
+                    "&tw13="
+                    "&yczk.dm=01"
+                    "&yczk1=无症状"
+                    "&fbrq="
+                    "&jzInd=0"
+                    "&jzYy="
+                    "&zdjg="
+                    "&fxrq="
+                    "&brStzk.dm=01"
+                    "&brStzk1=身体健康、无异常"
+                    "&brJccry.dm=01"
+                    "&brJccry1=未接触传染源"
+                    "&jrStzk.dm=01"
+                    "&jrStzk1=身体健康、无异常"
+                    "&jrJccry.dm=01"
+                    "&jrJccry1=未接触传染源"
+                    "&jkm=1"
+                    "&jkm1=绿色"
+                    "&xcm=1"
+                    "&xcm1=绿色"
+                    "&xgym="
+                    "&xgym1="
+                    "&hsjc=1"
+                    "&hsjc1="
+                    "&bz="
+                    "&operationType=Create"
+                    "&dm=")
+    post_data=post_data.encode('utf-8')
+    rsp = requests.post(url,headers=headers, data=post_data,cookies=cookies).json()
+    return rsp
+def ts():
+    key = "SCT114092TdaLtRYOnxp0vLc0wwZAau4V6"  # 微信推送key这里    链接获取#https://sct.ftqq.com/sendkey
+    api = "https://sc.ftqq.com/" + key + ".send"
+    data = {
+        "title": "打卡成功",
+        "desp":str(post_xxx())
+    }
+    reqq = requests.post(api, data=data).json()
+    return reqq
+print(ts())
 def main_handler(event, context):
-    r = "1"
-    return r
+    print(post_xxx())
 if __name__ == '__main__':
-    post()
+
