37e88eaecf55ddec506d6d08a6c2f681f7a63d0e
月初 <85841713+787078720@users.noreply.github.com>
2023-01-04 13:24:26 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,17 +1,15 @@
-####代码仅供学习参考，
-####by 月初
+####代码仅供学习参考，by 月初
 import requests
 import time
 from datetime import datetime
 import hashlib
 import urllib3
 #-------------------------------------------------------变量信息----------------------------------------------------------------#
-
 zhh={
-    1:"202153",###第一个账号
+    1:"2021",###第一个账号
 }
 mmm={
-    1: "密码",###第一个账号密码
+    1: "",###第一个账号密码
 }
 n=0+1
 zs = len(zhh)
@@ -31,19 +29,7 @@ while n<zs+1:
     kk = k + "a" + l + "b" + m
     n=n+1
     # -------------------------------------------------登录提取cookie--------------------------------------------------------------#
-    headersq = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.46',
-        'Host': 'xggl.hnqczy.com',
-        'Connection': 'keep-alive',
-        'Content-Length': '57',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Origin': 'http://xggl.hnqczy.com',
-        'Referer': 'http://xggl.hnqczy.com/index;jsessionid=E4C1D2DFA72761FF80084C3175023A8D',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6'
-    }
+    headersq = {}
     urlq = "http://xggl.hnqczy.com/website/login"
     dataq = [
         ("uname", zh),
@@ -58,24 +44,16 @@ while n<zs+1:
     # ---------------- ---------------------------------获取上次打卡记录josn--------------------------------------------------------------#
 def get_xxx():
     url="http://xggl.hnqczy.com/content/student/temp/zzdk/lastone?_t_s_=1661221850154"
-    headers = {'Host': 'xggl.hnqczy.com',
-               'Connection': 'keep-alive',
-               'Accept': '*/*',
-               'User-Agent': 'Mozilla/5.0 (Linux; Android 12; M2012K11AC Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36;webank/h5face;webank/1.0 yiban_android/5.0.12',
-               'X-Requested-With': 'XMLHttpRequest',
-               'Referer': 'http://xggl.hnqczy.com/wap/menu/student/temp/zzdk/_child_/edit?_t_s_=1661221849267',
-               'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-               }
+    headers = {}
     cookies = {
         'JSESSIONID':cooick1,
     }
     rsp =requests.get(url, headers,cookies=cookies).json()
     return rsp
-dkdz=get_xxx()['dkdz']
-dkd=get_xxx()['dkd']
-jzdDz2=get_xxx()['jzdDz2']
-lxdh = get_xxx()["lxdh"]
-print(get_xxx())
+hqjl=get_xxx()
+dkd=hqjl['dkd']
+jzdDz2=hqjl['jzdDz2']
+lxdh = hqjl["lxdh"]
 # ----------------------------------------------------------校验加密--------------------------------------------------------------#
 def jm():
     http = urllib3.PoolManager()
@@ -92,21 +70,12 @@ print(jm())
 # ----------------------------------------------------------打卡--------------------------------------------------------------#
 def post_xxx():
     url="http://xggl.hnqczy.com/content/student/temp/zzdk?_t_s_=1661311939332"
-    headers = {'Host': 'xggl.hnqczy.com',
-               'Connection': 'keep-alive',
-               'Accept': '*/*',
-               'X-Requested-With': 'XMLHttpRequest',
-               'User-Agent': 'Mozilla/5.0 (Linux; Android 12; M2012K11AC Build/SKQ1.211006.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.104 Mobile Safari/537.36;webank/h5face;webank/1.0 yiban_android/5.0.12',
-               'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-               'Origin': 'http://xggl.hnqczy.com',
-               'Referer': 'http://xggl.hnqczy.com/wap/menu/student/temp/zzdk/_child_/edit?_t_s_=1661311908508',
-               'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-               }
+    headers = {}
     cookies = {
         'JSESSIONID':cooick1,
     }
-    post_data = str("dkdz="+dkdz+
-                    "&dkdzZb=111.795036,28.592264"
+    post_data = str("dkdz="+dkd+
+                    "&dkdzZb=111.795074%2C28.592226"
                     "&dkly=yiban"
                     "&zzdk_token="+str(jm())+
                     "&dkd="+dkd+
@@ -155,15 +124,13 @@ def post_xxx():
     post_data=post_data.encode('utf-8')
     rsp = requests.post(url,headers=headers, data=post_data,cookies=cookies).json()
     return rsp
-xi=str(post_xxx())
-if "True" in xi:
-    tt = "打卡成功"
+if post_xxx()['result']=='true':
+    tt="打卡成功"
 else:
-    tt = '打卡失败'
-print(tt)
+    tt="打卡失败"
 # ----------------------------------------------------------推送--------------------------------------------------------------#
 def ts():
-    key = "填写"  # 微信推送key这里    链接获取#https://sct.ftqq.com/sendkey
+    key = ""  # 微信推送key这里    链接获取#https://sct.ftqq.com/sendkey
     api = "https://sc.ftqq.com/" + key + ".send"
     data = {
         "title": str(tt),
@@ -173,5 +140,7 @@ def ts():
     return reqq
 print(ts())
 def main_handler(event, context):
-    if __name__ == '__main__':
-        print("1")
+    print(post_xxx())
+if __name__ == '__main__':
+    print(post_xxx())
+
